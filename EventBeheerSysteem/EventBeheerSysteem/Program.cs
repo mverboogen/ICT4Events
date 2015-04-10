0364531f5747f500da687e38f39517a7fe2a1d35
mverboogen <kaarten_verboogen@hotmail.com>
2015-04-10 16:09:08 +0200

    Revert "EventBeheerSysteem merged with UI"
    This reverts commit 209404e041d47261e60a62236f647ba78214b8f9.

--- EventBeheerSysteem/EventBeheerSysteem/Program.cs
+++ EventBeheerSysteem/EventBeheerSysteem/Program.cs
@@ -16,7 +16,7 @@ namespace EventBeheerSysteem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new EbsHomeForm());
+            Application.Run(new EBSForm());
         }
     }
 }
